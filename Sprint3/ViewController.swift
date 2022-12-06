ebeb1fc558899bfdfca7090633d8a9a42820913c
Alexey Litvinov <maillit@icloud.com>
2022-12-06 13:31:50 +0700

    Initial Commit

---
+++ Sprint3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sprint3
+//
+//  Created by Alexey on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
