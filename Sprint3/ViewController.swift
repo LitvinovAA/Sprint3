6d61a0c4e4316507e64c7bb869cd3e801a544627
Alexey Litvinov <maillit@icloud.com>
2022-12-06 22:42:08 +0700

    Finish Edit

--- Sprint3/ViewController.swift
+++ Sprint3/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeCountButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    
+   var userScore: Int = 0
+    var scoreText: String {
+        "Значение счетчика: \n\(userScore)"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countLabel.text = scoreText
+    }
+    
+
+    @IBAction func buttonAdd(_ sender: Any) {
+        userScore += 1
+        countLabel.text = scoreText
     }
 
+    
+  
 
 }
 
